7714fa77e6eb4bdb969e387463e0e513b910969e
Av981 <avfullstack8@gmail.com>
2022-04-29 06:12:51 +0530

    create a form and validate it with the help of yup and regex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import "./App.css";
+
+const FormSchema = yup.object().shape({
+  name: yup.string()
+       .required( "Name is required")
+       .matches("^[A-Za-z]+$", "Enter a valid name")
+       .min(4).max(20),
+  phone: yup.string()
+        .required("Phone Number is Required")
+        .matches(
+        "^[0-9]{10}$",
+        "Phone number is not valid"
+      ),
+  age: yup.number("Invalid Age")
+      .required( "Age is required")
+      .min(0).max(100),
+  email: yup.string().email("Invalid email format").required("Email is required")
+});
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(FormSchema)
+  });
+  const onSubmit = (data) => {
+
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h4>Form</h4>
+      <div>
+        <label> Name</label>
+        <input {...register("name")} />
+        {errors.name && <p>{errors.name.message}</p>}
+      </div>
+      <div>
+        <label>Phone Number</label>
+        <input type='number' {...register("phone")} />
+        {errors.phone && <p>{errors.phone.message}</p>}
+      </div>
+      <div>
+        <label>Age</label>
+        <input type='number' {...register("age",{ valueAsNumber: true })} />
+        {errors.age && <p>{errors.age.message}</p>}
+      </div>
+      <div>
+        <label>E-mail</label>
+        <input {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+      </div>
+      <input type="submit" />
+    </form>
   );
 }
 
 export default App;
+
